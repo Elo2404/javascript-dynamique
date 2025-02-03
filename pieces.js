04b9b8858ee0b90b8b89b29f07803127aa339d43
Elo2404 <elo.dol19@gmail.com>
2025-02-03 10:01:42 +0100

    Update pieces.js

--- pieces.js
+++ pieces.js
@@ -1,6 +1,7 @@
 // Récupération des pièces depuis le fichier JSON
 const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
+console.log(pieces);
 
 for (let i = 0; i < pieces.length; i++) {
 
